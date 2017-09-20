cacd024bffa9f444fa922a71aae07dd68271825a
Robert Willemelis <r.wille3055@gmx.de>
2017-09-20 14:38:00 +0200

    task 3 - add title box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
 import { MousePositionComponent } from './mouse-position/mouse-position.component';
+import { TitleBoxComponent } from './title-box/title-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InfoBoxComponent,
-    MousePositionComponent
+    MousePositionComponent,
+    TitleBoxComponent
   ],
   imports: [
     BrowserModule
