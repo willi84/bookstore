f4987bbac4f4c267ba05bec8532db4e7a7be6b84
Robert Willemelis <r.wille3055@gmx.de>
2017-09-20 13:35:45 +0200

    task 1 - add infobox to show and hide

---
+++ src/app/info-box/info-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'info-box',
+  templateUrl: './info-box.component.html',
+  styleUrls: ['./info-box.component.css']
+})
+export class InfoBoxComponent implements OnInit {
+
+  public isHidden: boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
