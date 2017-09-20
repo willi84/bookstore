23f679a398d08620528fbd6e2b13f654cff7b69c
Robert Willemelis <r.wille3055@gmx.de>
2017-09-20 15:41:13 +0200

    task 5 - add booklist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
 import { MousePositionComponent } from './mouse-position/mouse-position.component';
 import { TitleBoxComponent } from './title-box/title-box.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InfoBoxComponent,
     MousePositionComponent,
-    TitleBoxComponent
+    TitleBoxComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule
